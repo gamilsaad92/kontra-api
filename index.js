e8e350864cf77eb6c50ed17f16d2f3b41309758a
GS <gs@MacBookPro.home>
2025-04-14 20:21:34 -0700

    Add /api/draw-request endpoint

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Kontra API is running');
 });
+app.post('/api/draw-request', (req, res) => {
+  const { project, amount, description } = req.body;
+  console.log("Received draw:", { project, amount, description });
+  res.json({ message: `Draw request received for ${project}` });
+});
 
 app.listen(5000, () => {
   console.log('Kontra API listening on port 5000');
